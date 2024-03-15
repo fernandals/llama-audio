e74bca273c2f8ea505ebd4facda8d6d1a6171ef3
Fernanda Lustosa <lustosafk@gmail.com>
2024-03-14 22:57:11 -0300

    run job model

---
+++ run_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+#SBATCH --job-name=llama2_test
+#SBATCH --time=1-23:58
+#SBATCH --partition=gpu-4-a100
+#SBATCH --gres=gpu:1
+
+# informando ao tch-rs que desejo compilar com cuda na versão 11.7
+export TORCH_CUDA_VERSION=cu117
+
+python3 example_chat_completion.py llama-2-7b/ tokenizer.model

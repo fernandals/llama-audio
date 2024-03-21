7f7c7eb715d6fa579bed289d10c5833ce9a06c7e
Fernanda Lustosa <lustosafk@gmail.com>
2024-03-21 16:43:20 -0300

    testing with the number of cpus per tasks

--- run_job.sh
+++ run_job.sh
@@ -3,6 +3,7 @@
 #SBATCH --time=1-23:58
 #SBATCH --partition=gpu-4-a100
 #SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
 
 # informando ao tch-rs que desejo compilar com cuda na versão 11.7
 export TORCH_CUDA_VERSION=cu117

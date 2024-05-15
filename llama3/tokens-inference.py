c8e135b4d4be29f072d94f1e51fc3b23e344ade5
Fernanda Lustosa <lustosafk@gmail.com>
2024-05-15 20:22:49 -0300

    llama3 scripts execution

--- llama3/tokens-inference.py
+++ llama3/tokens-inference.py
@@ -1,6 +1,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
 
+# script to infere on llama3 using audio tokens
+
 from typing import List
 
 import fire
@@ -18,9 +20,6 @@ def main(
     max_batch_size: int = 4,
 ):
     """
-    Examples to run with the pre-trained models (no fine-tuning). Prompts are
-    usually in the form of an incomplete text prefix that the model can then try to complete.
-
     The context window of llama3 models is 8192 tokens, so `max_seq_len` needs to be <= 8192.
     `max_gen_len` is needed because pre-trained models usually do not stop completions naturally.
     """
@@ -31,25 +30,11 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-    prompts: List[str] = [
-        # For these prompts, the expected answer is the natural continuation of the prompt
-        "I believe the meaning of life is",
-        "Simply put, the theory of relativity states that ",
-        """A brief message congratulating the team on the launch:
-
-        Hi everyone,
-
-        I just """,
-        # Few shot prompt (providing a few examples before asking model to complete more);
-        """Translate English to French:
-
-        sea otter => loutre de mer
-        peppermint => menthe poivrée
-        plush girafe => girafe peluche
-        cheese =>""",
-    ]
+    prompts: List[str] = ["I believe the meaning of life is"]
+    prompt_tokens = [generator.tokenizer.encode(x, bos=True, eos=False) for x in prompts]
+    
     results = generator.text_completion(
-        prompts,
+        prompt_tokens,
         max_gen_len=max_gen_len,
         temperature=temperature,
         top_p=top_p,
--- run_job.sh
+++ run_job.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
 #SBATCH --job-name=llama_run
 #SBATCH --time=0-30:00
-#SBATCH --partition=gpu-4-a100
+#SBATCH --partition=gpu-8-v100
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=10
 
 # informando ao tch-rs que desejo compilar com cuda na versão 11.7
 export TORCH_CUDA_VERSION=cu117
 
-torchrun llama2/hub-inf.py llama2/llama-2-7b/ llama2/tokenizer.model
+torchrun llama3/tokens-inference.py \
+	--ckpt_dir Meta-Llama-3-8B/ \
+	--tokenizer_path Meta-Llama-3-8B/tokenizer.model \
+	--max_sex_len 512
